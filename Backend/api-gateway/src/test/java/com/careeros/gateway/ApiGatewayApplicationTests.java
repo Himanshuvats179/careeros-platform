7496474865e16adb0fb1be73956062bb094f4854
himanshuvats179 <hh21csb0b222@student.nitw.ac.in>
2026-07-02 21:26:15 +0530

    chore: bootstrap CareerOS microservices foundation

---
+++ Backend/api-gateway/src/test/java/com/careeros/gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careeros.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
